ff1c8f3acef2af6839d23dc0806b9430daf68ae0
Deepak Kumar Bhardwaj <deepak@adeptmind.ai>
2023-03-20 14:17:48 +0530

    added github actions file

---
+++ actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import json
+import os
+import sys
+from django.contrib import admin
+from django.urls import path
+
+
+def run_actions():
+    print("##### CHECK FOR ENV VALUES")
+    mms_url = os.environ["MMS_URL"]
+    print(f"Fetched MMS_URL value is {mms_url}")
+
+    print("##### CHECK FOR INSTALLED REQUIREMENTS")
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+    ]
+    print(f"urlpatterns created are - {urlpatterns}")
+
+    print("####### Add a sample file.")
+    data = {
+        "morguard": "en_ca",
+        "hammerson": "fr_fr"
+    }
+    with open("sample_config.json", "w") as f:
+        json.dump(data, f)
+
+
+if __name__ == '__main__':
+    run_actions()
